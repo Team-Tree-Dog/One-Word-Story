7ca513c046e4b81c4aff5b998a3ca837f42f1b8f
cotopaxi64 <daniel.honciuc@mail.utoronto.ca>
2022-10-27 12:06:25 -0400

    Added PlayerFactory, as well as its JavaDoc

---
+++ src/main/java/entities/PlayerFactory.java
@@ -0,0 +1,22 @@
+package entities;
+
+import exceptions.IdInUseException;
+
+import java.util.ArrayList;
+
+public class PlayerFactory {
+
+    private static ArrayList<String> idsInUse;
+
+    /**
+     * Accepting any settings, create the appropriate game instance of the GameRegular
+     * @param newPlayerId the ID of the new player.
+     * @param newName the name of the new player.
+     * @return the created Player instance
+     */
+    public Player createPlayer(String newName, String newPlayerId) throws IdInUseException {
+        if (idsInUse.contains(newPlayerId)) {throw new IdInUseException("ID " + newPlayerId + " already in use.");}
+        idsInUse.add(newPlayerId);
+        return new Player(newName, newPlayerId);
+    }
+}
