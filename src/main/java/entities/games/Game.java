46acfca6d2e72238fc805c5946306bbb6232eb2b
tostistos <fidler.patrick@gmail.com>
2022-10-29 03:30:41 -0400

    Added documentation to Game, GameRegular

--- src/main/java/entities/games/Game.java
+++ src/main/java/entities/games/Game.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * An abstract game
+ * Every type of game extends this class
+ */
 public abstract class Game {
     private Timer gameTimer;
     private OnTimerUpdateBoundary onTimerBound;
@@ -20,6 +24,14 @@ public abstract class Game {
     private int secondsLeftInCurrentTurn;
     private boolean gameEnded;
 
+    /**
+     * Constructor for a Game
+     * @param initialPlayers The initial players in this Game
+     * @param otub The timer update boundary
+     * @param geb The game ended boundary
+     * @param secondsPerTurn The amount of seconds for each turn
+     * @param v The validity checker (to check if a word is valid)
+     */
     public Game(List<Player> initialPlayers, OnTimerUpdateBoundary otub,
                 GameEndedBoundary geb, int secondsPerTurn, ValidityChecker v) {
         this.onTimerBound = otub;
@@ -32,6 +44,11 @@ public abstract class Game {
         }
     }
 
+    /**
+     * Starts the game's timer
+     * Tracks how many seconds are left in the current turn, and switches turns when time has run out
+     * If the game is over, the timer stops
+     */
     protected void startTimer() {
         this.secondsLeftInCurrentTurn = secondsPerTurn;
         gameTimer.scheduleAtFixedRate(new TimerTask() {
@@ -52,23 +69,47 @@ public abstract class Game {
         }, 0, 500);
     }
 
+    /**
+     * @return The story as it has been typed in this game instance
+     */
     public Story getStory() {return story;}
+
     public OnTimerUpdateBoundary getOnTimerBound() {return onTimerBound;}
+
     public GameEndedBoundary getGameEndBound() {return gameEndBound;}
+
     public int getSecondsPerTurn() {return secondsPerTurn;}
+
     public int getSecondsLeftInCurrentTurn() {return secondsLeftInCurrentTurn;}
+
+    /**
+     * Sets the seconds left in the current turn
+     * @param newSeconds The amount of seconds to set the current turn for
+     */
     public void setSecondsLeftInCurrentTurn(int newSeconds) {
         secondsLeftInCurrentTurn = newSeconds;
     }
+
+    /**
+     * @return if the game has ended
+     */
     public boolean isGameEnded() {return gameEnded;}
+
     public abstract List<Player> getPlayers();
 
     protected abstract void onTimerUpdate();
+
     public abstract Player getPlayerById(String PlayerId);
+
     public abstract boolean removePlayer(Player playerToRemove);
+
     public abstract boolean addPlayer(Player playerToAdd);
+
     public abstract boolean switchTurn();
+
     public abstract void modifyTurnTime();
+
     public abstract Player getCurrentTurnPlayer();
+
     protected abstract boolean isGameOver();
 }
--- src/main/java/entities/games/GameRegular.java
+++ src/main/java/entities/games/GameRegular.java
@@ -10,9 +10,19 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Queue;
 
+/**
+ * A game, with the "regular" rules and constraints
+ */
 public class GameRegular extends Game {
 
     private LinkedList<Player> players = new LinkedList<Player>();
+
+    /**
+     * Constructor for GameRegular
+     * @param initialPlayers The initial players in this GameRegular
+     * @param otub The timer update boundary
+     * @param geb The game ended boundary
+     */
     public GameRegular(List<Player> initialPlayers, OnTimerUpdateBoundary otub,
                        GameEndedBoundary geb) {
         super(initialPlayers, otub, geb, 15, new ValidityChecker() {
@@ -29,9 +39,16 @@ public class GameRegular extends Game {
         return players;
     }
 
+    /**
+     * Does PUT_THINGS_HERE when the timer updates
+     */
     @Override
     protected void onTimerUpdate() {}
 
+    /**
+     * @param PlayerId The unique id of the player to search for in this GameRegular instance.
+     * @return The Player with the corresponding PlayerId, or null if this player does not exist
+     */
     @Override
     public Player getPlayerById(String PlayerId) {
         for (Player player : this.getPlayers()) {
@@ -42,35 +59,58 @@ public class GameRegular extends Game {
         return null;
     }
 
+    /**
+     * Removes the player specified from this GameRegular instance
+     * @param playerToRemove The Player to be removed
+     * @return if the player was successfully removed
+     */
     @Override
     public boolean removePlayer(Player playerToRemove) {
         return players.remove(playerToRemove);
     }
 
+    /**
+     * Adds the player specified to this GameRegular instance
+     * @param playerToAdd The Player to be added
+     * @return if the player was successfully added
+     */
     @Override
     public boolean addPlayer(Player playerToAdd) {
-        players.add(playerToAdd);
-        return true;
+        return players.add(playerToAdd);
     }
 
+    /**
+     * Moves the player whose turn it currently is from the front of the list of players to the back
+     * It is now the new player in the front's turn
+     * @return if the turn switch was successful
+     */
     @Override
     public boolean switchTurn() {
-        players.add(players.remove());
-        return true;
+        return players.add(players.remove());
     }
 
+    /**
+     * Modifies the current amount of seconds left in current turn to the amount of seconds per turn
+     */
     @Override
     public void modifyTurnTime() {
         this.setSecondsLeftInCurrentTurn(getSecondsPerTurn());
     }
 
+    /**
+     * @return the first player in the player list
+     */
     @Override
     public Player getCurrentTurnPlayer() {
         return players.peek();
     }
 
+    /**
+     * Checks if the amount of players in the game is less than 2, which means the game is over
+     * @return if the game is over
+     */
     @Override
     protected boolean isGameOver() {
-        return players.size() == 1;
+        return players.size() < 2;
     }
 }
