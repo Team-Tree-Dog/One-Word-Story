fb8d1f5d87e579be96b5da4d05af353a432bd65b
cotopaxi64 <daniel.honciuc@mail.utoronto.ca>
2022-10-30 10:57:54 -0400

    Added the test classes structures, with setup and teardown. No test cases yet.

---
+++ src/test/java/entities/games/GameFactoryRegularTests.java
@@ -0,0 +1,17 @@
+package entities.games;
+
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class GameFactoryRegularTests {
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
