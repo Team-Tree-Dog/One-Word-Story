8bb9a188adc5da51f8c74c7c4ca6aa2834c24343
Mariana Piz <marianapiz0412@gmail.com>
2022-10-29 23:11:44 -0400

    Added JavaDoc to exceptions

--- src/main/java/exceptions/IdInUseException.java
+++ src/main/java/exceptions/IdInUseException.java
@@ -1,7 +1,16 @@
 package exceptions;
 
+/**
+ * Exception for a case when ID under which the user wants to register is already taken by another user
+ */
 public class IdInUseException extends Exception {
+
+    /**
+     * Constructor for IdInUseException
+     * @param str message which will be displayed when exception is called
+     */
     public IdInUseException(String str) {
         super(str);
     }
+
 }
--- src/main/java/exceptions/InvalidDisplayNameException.java
+++ src/main/java/exceptions/InvalidDisplayNameException.java
@@ -1,7 +1,16 @@
 package exceptions;
 
+/**
+ * Exception for a case when the user's name is invalid
+ */
 public class InvalidDisplayNameException extends Exception {
+
+    /**
+     * Constructor for InvalidDisplayNameException
+     * @param str message which will be displayed when exception is called
+     */
     public InvalidDisplayNameException(String str) {
         super(str);
     }
+
 }
