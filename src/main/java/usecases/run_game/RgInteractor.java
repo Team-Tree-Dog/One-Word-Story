033a50342d3e068de9d12202b84ee9c2829b1a5e
Alexander Ryabchenko <alexander.ryabchenko@mail.utoronto.ca>
2022-11-05 11:16:58 -0400

    Fixes

--- src/main/java/usecases/run_game/RgInteractor.java
+++ src/main/java/usecases/run_game/RgInteractor.java
@@ -11,49 +11,61 @@ import java.util.TimerTask;
 public class RgInteractor {
 
     private final Game g;
-    private final JplOutputBoundary p;
     private final PgeOutputBoundary pge;
     private final PdInputBoundary pd;
 
     /**
      * @param g Game that we interact with
-     * @param p Join Public Lobby use-case output boundary
-     * @param pge Pull Game Ended use-case output boundary
-     * @param pd Pull Data use-case input boundary
+     * @param pge "Pull Game Ended" use-case output boundary
+     * @param pd "Pull Data" use-case input boundary
      */
-    public RgInteractor (Game g, JplOutputBoundary p, PgeOutputBoundary pge, PdInputBoundary pd) {
+    public RgInteractor (Game g, PgeOutputBoundary pge, PdInputBoundary pd) {
         this.g = g;
-        this.p = p;
         this.pge = pge;
         this.pd = pd;
     }
 
-    public static class RgTask extends TimerTask {
 
-        private final Game game;
-        public RgTask (Game game) { // TODO: check with Alex
-            this.game = game;
-        }
+    /**
+     * Internal Timer Task to be used within the main Timer below
+     */
+    public class RgTask extends TimerTask {
 
+        /**
+         * Method to override from abstract TimerTask
+         */
         @Override
         public void run () {
-            if (game.isGameOver()) {
-                game.getGameTimer().cancel();
-                pge.onGameEnded(new PgeInputData(game.getPlayers()));
-                game.setTimerStopped();
+            if (RgInteractor.this.g.isGameOver()) {
+                // Game ending procedure:
+
+                // Cancel the timer
+                g.getGameTimer().cancel();
+
+                // Perform "Game Ended" use-case
+                pge.onGameEnded(new PgeInputData(g.getPlayers()));
+
+                // Notify the game, after timer cancellation, when the
+                // last execution of the run method has finished, meaning,
+                // no more game timer code will run
+                g.setTimerStopped();
+
             } else {
-                game.setSecondsLeftInCurrentTurn(game.getSecondsPerTurn() - 1);  // TODO: implement this in Game class
-                if (game.getSecondsLeftInCurrentTurn() == -1) {
-                    game.switchTurn();
+                // Regular procedure:
+
+                //
+                g.setSecondsLeftInCurrentTurn(g.getSecondsPerTurn() - 1);
+                if (g.getSecondsLeftInCurrentTurn() == -1) { // We want to display 0 before, it is over
+                    g.switchTurn();
                 }
-                game.onTimerUpdate();
-                pd.onTimerUpdate(game);
+                g.onTimerUpdate();
+                pd.onTimerUpdate(g);
             }
         }
     }
 
     public void startTimer () {
-        this.g.getGameTimer().schedule(new RgTask(this.g), 1000, 1000);
+        this.g.getGameTimer().schedule(new RgTask(), 1000, 1000);
     }
 
 }
