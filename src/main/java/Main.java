3ed4620f8d86c837575d199c07fb09759501ac7b
Aleksey Panas <panasaleksey3@gmail.com>
2022-11-05 09:50:05 -0400

    Add blank main method

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Orchestrator. Contains only a main method which boots up
+ * the server.
+ */
+public class Main {
+
+    /**
+     * Builds the clean arch structure and initializes it.
+     * @param args Command line arguments (currently none necessary)
+     */
+    public static void main (String[] args) {
+
+    }
+}
