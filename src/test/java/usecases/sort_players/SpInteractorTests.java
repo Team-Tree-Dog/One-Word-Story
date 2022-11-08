645b3fff56e7d7f956e5f864191fed96b7cc9101
AlekseyPanas <panasaleksey3@gmail.com>
2022-11-07 23:36:30 -0500

    Create test file

---
+++ src/test/java/usecases/sort_players/SpInteractorTests.java
@@ -0,0 +1,39 @@
+package usecases.sort_players;
+
+import org.junit.After;
+import org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class SpInteractorTests {
+
+    /**
+     *
+     */
+    @Before
+    public void setup () {
+
+    }
+
+    /**
+     *
+     */
+    @After
+    public void teardown () {
+
+    }
+
+    /**
+     * Test the scenario where two players are in the pool. Sort players
+     * should in this case empty the players from the pool into a new game
+     * and start the game timer.
+     */
+    @Test(timeout=1000)
+    public void testTwoPlayersInPoolStartGame () {
+
+    }
+    
+}
