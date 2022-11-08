796eac4956786b2d45db0c75a3ae9ee8a98d040f
AlekseyPanas <panasaleksey3@gmail.com>
2022-11-08 09:41:36 -0500

    WIP Test Method

--- src/test/java/usecases/sort_players/SpInteractorTests.java
+++ src/test/java/usecases/sort_players/SpInteractorTests.java
@@ -1,9 +1,17 @@
 package usecases.sort_players;
 
+import entities.DisplayNameChecker;
+import entities.LobbyManager;
+import entities.PlayerFactory;
+import entities.games.GameFactoryRegular;
 import org.junit.After;
 import org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import usecases.join_public_lobby.*;
+import usecases.pull_data.PdInteractor;
+import usecases.pull_data.PdOutputBoundary;
+import usecases.pull_data.PdOutputData;
 
 /**
  *
@@ -33,7 +41,47 @@ public class SpInteractorTests {
      */
     @Test(timeout=1000)
     public void testTwoPlayersInPoolStartGame () {
+        PdInteractor pd = new PdInteractor(new PdOutputBoundary() {
+            @Override
+            public void updateGameInfo(PdOutputData d) {
+
+            }
+        });
+        PgeInteractor pge = new PgeInteractor(new PgeOutputBoundary() {
+            @Override
+            public void notifyGameEnded (PgeOutputData d) {
+
+            }
+        });
+        LobbyManager m = new LobbyManager(new PlayerFactory(new DisplayNameChecker() {
+            @Override
+            public boolean checkValid(String displayName) {
+                return true;
+            }
+        }), new GameFactoryRegular());
+
+        SpInteractor sp = new SpInteractor(m, pge, pd);
+
+        SpInteractor.SpTask spTimerTask = sp.new SpTask();
+    }
+
+    /**
+     * Test the scenario where the game is not null but is over. Sort players
+     * should set the game to null
+     */
+    @Test(timeout=1000)
+    public void testGameNotNullIsOverSetNull () {
 
     }
-    
+
+    /**
+     * Test the scenario where the game is not null and not over and players
+     * are in the pool. Sort players should try to add the players to the game
+     * and remove them from the pool. The players will be successfully added due
+     * to the game implementation used for this test
+     */
+    @Test(timeout=1000)
+    public void testGameRunningPlayersInPool () {
+
+    }
 }
