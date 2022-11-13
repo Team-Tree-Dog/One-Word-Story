c192ba0eb86534cbcdb602f03fd85ef21625e778
tostistos <fidler.patrick@gmail.com>
2022-11-13 18:59:26 -0500

    Added PgeInteractorTests, fixed PgeInteractor

--- src/main/java/usecases/pull_game_ended/PgeInteractor.java
+++ src/main/java/usecases/pull_game_ended/PgeInteractor.java
@@ -1,5 +1,9 @@
 package usecases.pull_game_ended;
 
+import entities.Player;
+
+import java.util.List;
+
 /**
  * Interactor for the Pull Game Ended Data use case
  */
@@ -16,20 +20,28 @@ public class PgeInteractor implements PgeInputBoundary {
     }
 
     /**
+     * Method to return the list of player ids from a list of players
+     * @param players contains a list of players
+     * @return a list of player ids
+     */
+    public String[] getPlayerIds(List<Player> players) {
+        int numberOfPlayers = players.size();
+        String[] playerIds = new String[numberOfPlayers];
+        for (int i = 0; i < numberOfPlayers; i++) {
+            playerIds[i] = players.get(i).getPlayerId();
+        }
+        return playerIds;
+    }
+
+    /**
      * Notifies presenter of player ids that need to be notified that the game has ended
      * FUTURE: Updates database about something
      * @param data game statistics, including players in the game that ended
      */
     @Override
     public void onGameEnded(PgeInputData data) {
-        int numberOfPlayers = data.players.size();
-        String[] playerIds = new String[numberOfPlayers];
-
-        for (int i = 0; i < numberOfPlayers; i++) {
-            playerIds[i] = data.players.get(i).getPlayerId();
-        }
-
-        PgeOutputData idsToBeNotified = new PgeOutputData(playerIds);
-        presenter.notifyGameEnded(idsToBeNotified);
+        presenter.notifyGameEnded(
+                new PgeOutputData(getPlayerIds(data.players))
+        );
     }
 }
---
+++ src/test/java/usecases/pull_game_ended/PgeInteractorTests.java
@@ -0,0 +1,49 @@
+package usecases.pull_game_ended;
+
+import entities.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+public class PgeInteractorTests {
+
+    // PgeInputBoundary, PgeInputData used to test PgeInteractor
+    private PgeInputBoundary pgeib;
+    private PgeInputData pgeid;
+
+    @Before
+    public void setUp() {
+        // Add players for a list of players
+        Player p1 = new Player("p1", "1");
+        Player p2 = new Player("p2", "2");
+
+        // Instantiate pgeInputBoundary
+        pgeib = new PgeInteractor(new PgeOutputBoundary() {
+            @Override
+            public void notifyGameEnded(PgeOutputData data) {}
+        });
+
+        // Instantiate pgeInputData with list of players
+        pgeid = new PgeInputData(new ArrayList<Player>(2));
+        pgeid.players.add(0, p1);
+        pgeid.players.add(1, p2);
+    }
+
+    @After
+    public void tearDown() {}
+
+    /**
+     * Test that the data is built correctly (the proper player ids were extracted)
+     */
+    @Test
+    public void testBuildData() {
+        String[] expectedPlayerIds = new String[2];
+        expectedPlayerIds[0] = "1";
+        expectedPlayerIds[1] = "2";
+        String[] actualPlayerIds = ((PgeInteractor) pgeib).getPlayerIds(pgeid.players);
+        assertEquals(expectedPlayerIds, actualPlayerIds);
+    }
+}
