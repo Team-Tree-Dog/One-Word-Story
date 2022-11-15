4e96129f14a5880f7f686345540cb162f5383a08
Aleksey Panas <panasaleksey3@gmail.com>
2022-11-15 14:22:34 -0500

    Change InputData to use getter

--- src/main/java/usecases/disconnecting/DcInputData.java
+++ src/main/java/usecases/disconnecting/DcInputData.java
@@ -5,11 +5,18 @@ package usecases.disconnecting;
  */
 public class DcInputData {
 
-    public String playerId;
+    private final String playerId;
 
     /**
      * Constructor for DcInputData
      * @param playerId id of player who needs to be disconnected
      */
     public DcInputData(String playerId) {this.playerId = playerId;}
+
+    /**
+     * @return id of player in this data
+     */
+    public String getPlayerId() {
+        return playerId;
+    }
 }
