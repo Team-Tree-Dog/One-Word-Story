16a85b1842bf66f2a3af1d75f1620fb7022d2960
cotopaxi64 <daniel.honciuc@mail.utoronto.ca>
2022-11-15 16:04:40 -0500

    Wrote and passed all the PlayerTests.

--- src/test/java/entities/PlayerTests.java
+++ src/test/java/entities/PlayerTests.java
@@ -1,8 +1,6 @@
 package entities;
 
 import org.junit.*;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +11,26 @@ public class PlayerTests {
 
     @After
     public void tearDown() {
+    }
+
+    /**
+     * Test that two players are equal for equal player IDs
+     */
+    @Test(timeout = 1000)
+    public void testEquals() {
+        Player player1 = new Player("player1", "1");
+        Player player2 = new Player("player2", "1");
+        // Notice how the display names are not the same.
+        assertEquals("The players should be the same because the IDs are the same.", player1, player2);
+    }
+
+    /**
+     * Test that two players are not equal for unequal player IDs
+     */
+    @Test(timeout = 1000)
+    public void testUnequals() {
+        Player player1 = new Player("player1", "1");
+        Player player2 = new Player("player2", "2");
+        assertNotEquals("The players should not be the same because the IDs are not the same.", player1, player2);
     }
 }
