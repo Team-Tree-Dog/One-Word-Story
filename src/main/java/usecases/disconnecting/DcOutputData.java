566baa5d2a1c76cf6cfe59f4c3eec5dcc0ff2101
Aleksey Panas <panasaleksey3@gmail.com>
2022-11-16 09:21:48 -0500

    Add playerID to output

--- src/main/java/usecases/disconnecting/DcOutputData.java
+++ src/main/java/usecases/disconnecting/DcOutputData.java
@@ -3,17 +3,22 @@ package usecases.disconnecting;
 import usecases.Response;
 
 /**
- * Output Data for Disconnecting Use Case
+ * Output Data for Disconnecting Use Case. A fail response is only given
+ * if the player was not found. Otherwise, the player is guaranteed to have
+ * been removed. In both cases, the player is guaranteed to be gone from
+ * the entities after the disconnect call
  */
 public class DcOutputData {
      private final Response response;
+     private final String playerId;
 
     /**
      * Constructor for DcOutputData
      * @param response Response which reports whether disconnecting was successful or not
      */
-    public DcOutputData(Response response) {
+    public DcOutputData(Response response, String playerId) {
          this.response = response;
+         this.playerId = playerId;
      }
 
     /**
@@ -22,4 +27,9 @@ public class DcOutputData {
     public Response getResponse() {
         return response;
     }
+
+    /**
+     * @return associated player ID
+     */
+    public String getPlayerId() { return playerId; }
 }
