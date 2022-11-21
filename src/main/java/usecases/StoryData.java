9b3ad997dacdaf0821f4e03942c0819d5c40aac5
sohail.sarkar <sohail.sarkar@mail.utoronto.ca>
2022-11-20 20:02:07 -0500

    Switch from Date to LocalDateTime

--- src/main/java/usecases/StoryData.java
+++ src/main/java/usecases/StoryData.java
@@ -2,12 +2,32 @@ package usecases;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class StoryData implements Comparable<StoryData> {
 
+    private String name;
     private String story;
     private String[] authorNames;
-    private Date publishTimeStamp;
+    private LocalDateTime publishTimeStamp;
+
+    public String getStory() {
+        return story;
+    }
+
+    public String[] getAuthorNames() {
+        return authorNames;
+    }
+
+    public LocalDateTime getPublishTimeStamp() {
+        return publishTimeStamp;
+    }
+
+    public String getName() {return name;}
+
+    public StoryData(String name, String[] authors, LocalDateTime dt1, String s1, int i) {
+
+    }
 
 
     @Override
