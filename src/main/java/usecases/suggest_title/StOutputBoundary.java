95fa9ee5bbd90eaf9ef628a241c8c1f51f153a3f
Joel <joelmathew.v2@gmail.com>
2022-12-01 00:43:14 -0500

    Implementation of output boundary and output data

--- src/main/java/usecases/suggest_title/StOutputBoundary.java
+++ src/main/java/usecases/suggest_title/StOutputBoundary.java
@@ -3,7 +3,7 @@ package usecases.suggest_title;
 /**
  * Output Boundary for the Suggest Title use case.
  */
-public interface StOutputBoundary {
+public interface StOutputBoundary{
     /**
      * Notifies the view model of the success of adding the suggested title to the database.
      * @param data The output data for the use case.
--- src/main/java/usecases/suggest_title/StOutputData.java
+++ src/main/java/usecases/suggest_title/StOutputData.java
@@ -1,4 +1,26 @@
 package usecases.suggest_title;
 
+import usecases.Response;
+
+/**
+ * The Output Data for this use case. Contains the ID corresponding to this particular title suggestion request
+ * and a Response containing the response code and description to be sent to the presenter.
+ */
 public class StOutputData {
+    String requestID;
+    Response res;
+
+    /**
+     * The constructor for this output data object.
+     * @param requestID the ID that is used to track this specific request by a user to suggest a story title
+     * @param res       the response object for this use case that will be sent to the presenter.
+     */
+    public StOutputData(String requestID, Response res){
+        this.requestID = requestID;
+        this.res = res;
+    }
+
+    public Response getRes() {return res;}
+
+    public String getRequestID() {return requestID;}
 }
