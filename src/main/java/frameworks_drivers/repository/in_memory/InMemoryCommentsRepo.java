7a17dcd8d301f6399b0d427af3d7fb75f41c40fc
Aleksey Panas <panasaleksey3@gmail.com>
2022-12-04 11:33:40 -0500

    Temp progress

--- src/main/java/frameworks_drivers/repository/in_memory/InMemoryCommentsRepo.java
+++ src/main/java/frameworks_drivers/repository/in_memory/InMemoryCommentsRepo.java
@@ -3,11 +3,43 @@ package frameworks_drivers.repository.in_memory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 /**
  * In memory implementation of the database component in charge of storing
  * comments associated with stories
  */
 public class InMemoryCommentsRepo implements GscGatewayComments, CagGatewayComments {
+
+    /**
+     * DB table row for storing a single comment entry
+     */
+    private static class CommentsTableRow {
+
+        private static int nextAvailableId = 0;
+        private final int commentId;
+        private final int storyId;
+        private final String displayName;
+        private String comment;
+
+        public CommentsTableRow (int storyId, @NotNull String displayName, @NotNull String comment) {
+            commentId = nextAvailableId;
+            nextAvailableId++;
+
+            this.storyId = storyId;
+            this.displayName = displayName;
+            this.comment = comment;
+        }
+
+        public String getDisplayName() { return displayName; }
+        public int getCommentId() { return commentId; }
+        public String getComment() { return comment; }
+        public int getStoryId() { return storyId; }
+
+        public void setComment(@NotNull String comment) { this.comment = comment; }
+    }
+
+    private final List<CommentsTableRow> commentsTable;
 
     /**
      *
@@ -18,7 +50,7 @@ public class InMemoryCommentsRepo implements GscGatewayComments, CagGatewayComme
      */
     @Override
     public boolean commentAsGuest (int storyId, @NotNull String displayName, @NotNull String comment) {
-
+        commentsTable.add()
     }
 
     /**
