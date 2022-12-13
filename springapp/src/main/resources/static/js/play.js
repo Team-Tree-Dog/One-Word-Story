ab057c034ec7defc39e6b24a605bc2487591f96e
Aleksey Panas <panasaleksey3@gmail.com>
2022-12-13 16:44:34 -0500

    Clean up frontend socket code

--- springapp/src/main/resources/static/js/play.js
+++ springapp/src/main/resources/static/js/play.js
@@ -1,3 +1,7 @@
+IS_SECURE = false
+DOMAIN = window.location.hostname + (!!window.location.port ? ":" + window.location.port : "")
+SOCKET_URL = "ws"+(IS_SECURE ? "s" : "")+"://"+DOMAIN+"/game"
+
 /**
  * Generate UUID
  * @returns A new UUID string
@@ -10,73 +14,18 @@ function uuidv4() {
 }
 
 /**
- * Callback for play button
+ * Callback for play button, initiates websocket
  * @param e event (ignored)
  */
 function onPlay(e) {
+    // Disable button so we don't start two connections if someone clicks it again quickly
     document.getElementById("play_button").disabled = true;
 
-    GameAPI = createAPI("ws://localhost:8080/game");
+    // Create API which initiates socket connection
+    GameAPI = createAPI(SOCKET_URL);
 
-    GameAPI.onReady(async function() {
-        GameAPI.log("TEST API READY");
-
-        const joinResult = await GameAPI.tryJoin(document.getElementById("name").value);
-        console.log("Join result: " + joinResult);
-
-        if (joinResult) {
-
-            switchToWaiting()
-
-            const intervalId = setInterval(async () => {
-                // Get the state every second
-
-                const updatedGameState = await GameAPI.getGameState();
-
-                if (!hasGameStarted && !!updatedGameState) {
-                    hasGameStarted = true;
-
-                    switchToGame();
-                }
-
-                if (hasGameStarted) {
-                    // Update frontend with new game data
-
-
-                    // Clear players list
-                    let ply_list = document.getElementById("players-list")
-                    ply_list.innerHTML = "";
-
-                    // BUILD PLAYER LIST
-                    updatedGameState.players.forEach(e => {
-                        // Build <li> element for player, with pencil icon if it's the player's turn
-                        let new_li = document.createElement("li")
-                        new_li.innerHTML = `<li>
-                                <div>
-                                    `+ e.displayName +
-                            (e.isCurrentTurnPlayer ? `<img style="vertical-align:middle" src="media/pencil.png" width="20" alt="Pencil">` : "") + `
-                                </div>
-                            </li>`
-
-                        ply_list.appendChild(new_li)
-                    })
-
-                    // POPULATE STORY
-                    document.getElementById("story").innerHTML = updatedGameState.storyString
-
-                    // POPULATE TIME
-                    document.getElementById("seconds-left").innerHTML = updatedGameState.secondsLeftInTurn
-
-
-                }
-
-                console.log("Current game state: ", updatedGameState);
-            }, 1000);
-        } else {
-            // Disconnect, display name invalid
-            window.location.reload();
-        }
-    });
+    // Wait for socket to be ready and then proceed with logic
+    GameAPI.onReady(socketLogic);
 }
 
 // SOCKET LOGIC
@@ -96,6 +45,67 @@ const RESPONSE_JOIN = "join_response";
 const RESPONSE_STATE = "current_state";
 
 /**
+ * Initiates and runs the socket logic once the connection has been established
+ */
+async function socketLogic () {
+    const joinResult = await GameAPI.tryJoin(document.getElementById("name").value);
+    console.log("Join result: " + joinResult);
+
+    if (joinResult) {
+
+        switchToWaiting()
+
+        const intervalId = setInterval(async () => {
+            // Get the state every second
+
+            const updatedGameState = await GameAPI.getGameState();
+
+            if (!hasGameStarted && !!updatedGameState) {
+                hasGameStarted = true;
+
+                switchToGame();
+            }
+
+            if (hasGameStarted) {
+                // Update frontend with new game data
+
+
+                // Clear players list
+                let ply_list = document.getElementById("players-list")
+                ply_list.innerHTML = "";
+
+                // BUILD PLAYER LIST
+                updatedGameState.players.forEach(e => {
+                    // Build <li> element for player, with pencil icon if it's the player's turn
+                    let new_li = document.createElement("li")
+                    new_li.innerHTML = `<li>
+                            <div>
+                                `+ e.displayName +
+                        (e.isCurrentTurnPlayer ? `<img style="vertical-align:middle" src="media/pencil.png" width="20" alt="Pencil">` : "") + `
+                            </div>
+                        </li>`
+
+                    ply_list.appendChild(new_li)
+                })
+
+                // POPULATE STORY
+                document.getElementById("story").innerHTML = updatedGameState.storyString
+
+                // POPULATE TIME
+                document.getElementById("seconds-left").innerHTML = updatedGameState.secondsLeftInTurn
+
+
+            }
+
+            console.log("Current game state: ", updatedGameState);
+        }, 1000);
+    } else {
+        // Disconnect, display name invalid
+        window.location.reload();
+    }
+}
+
+/**
  * Create and return the API object for interacting with
  * the websocket
  * @param url The websocket endpoint URL string
@@ -103,34 +113,62 @@ const RESPONSE_STATE = "current_state";
 function createAPI (url) {
     const apiObj = {
 
+        /**
+         * private websocket attribute
+         */
         _ws: {
-            isConnected: false,
+            isConnected: false, // Has connection been established
             wsHandle: null,
+            /**
+             * Stores a map of UUID to callback method.
+             * A client command adds a callback defining what to do when
+             * the server responds. Then it sends the message to the server. The
+             * callback will be called and deleted once the server replies
+             */
             messageHandlers: {},
 
-            init: function(url, mainHandler) {
-                this.messageHandlers = {
-                    "root": mainHandler
-                };
-
+            /**
+             * Initialize websocket object and define its event callbacks
+             * @param url socket url
+             */
+            init: function(url) {
                 this.wsHandle = new WebSocket(url);
-                this.wsHandle.onmessage = (data) => {
-                    // console.log("Active handlers: ", this.messageHandlers);
 
-                    (Object.values(this.messageHandlers) || []).filter(x => typeof x == 'function').forEach(x => x(data));
+                this.wsHandle.onmessage = (data) => {
+                    // Get all message handlers
+                    (Object.values(this.messageHandlers) || [])
+                        .filter(x => typeof x == 'function')
+                        .forEach(x => x(data));
                 };
+
                 this.wsHandle.onopen = () => {
-                    console.log("CONNECTED!");
+                    console.log("%c[Socket] %cConnection Established",
+                        "color:#8c1aff;font-weight:bold", "color:#ff99e6");
+
                     this.isConnected = true
                 };
+
                 this.wsHandle.onclose = () => {
-                    console.log("DISCONNECTED!");
+                    console.log("%c[Socket] %cConnection Closed",
+                        "color:#8c1aff;font-weight:bold", "color:#ff99e6");
                 };
             },
 
+            /**
+             * takes in elements of a desired message and joins them with
+             * agreed separator
+             */
             encode: (...elements) => (elements || []).join(SEPARATOR),
+            /**
+             * @param data Raw string payload from websocket
+             * @returns {string[]} Individual message elements split by separator
+             */
             decode: data => (data || "").split(SEPARATOR),
 
+            /**
+             * Send a raw payload to the server.
+             * @param message Raw payload to send
+             */
             send: function(message) {
                 if(!this.isConnected) {
                     throw "Invalid state";
@@ -140,23 +178,33 @@ function createAPI (url) {
             }
         },
 
-        apiUrl: url,
-        pendingCb: null,
-        log: function(message) { console.log(message) },
+        /** Logs messages in a pretty colored css format */
+        log: (m) => console.log("%c[Socket] %c" + m, "color:#8c1aff;font-weight:bold", "color:#ff99e6"),
 
+        /**
+         * Ensures that websocket is open before calling callback
+         * To safely use the socket, call this method and include all
+         * of your socket logic in the callback
+         * @param callback {function}
+         */
         onReady: function(callback) {
             const interval = setInterval(() => {
-                console.log("Checking connection");
-                if(this._ws.isConnected) {
-                    console.log("Connection established");
+                this.log("Checking connection...");
+                if (this._ws.isConnected) {
+                    this.log("Ready!");
                     clearInterval(interval);
                     callback();
                 }
             }, 50);
         },
 
+        /**
+         * JPL (displayName, uniqueId = socket session ID)
+         * @param playerName {string} desired display name
+         * @returns {Promise<boolean>}
+         */
         tryJoin: async function(playerName) {
-            return await new Promise((resolve, reject) => {
+            return new Promise((resolve, reject) => {
                 const waiterGuid = uuidv4();
 
                 this._ws.messageHandlers[waiterGuid] = (msg) => {
@@ -175,7 +223,7 @@ function createAPI (url) {
         },
 
         getGameState: async function(playerName) {
-            return await new Promise((resolve, reject) => {
+            return new Promise((resolve, reject) => {
                 const waiterGuid = uuidv4();
 
                 this._ws.messageHandlers[waiterGuid] = (msg) => {
@@ -202,10 +250,7 @@ function createAPI (url) {
         }
     };
 
-    apiObj._ws.init(
-        url,
-        (incoming) => {} //console.log("incoming:", incoming.data)
-    );
+    apiObj._ws.init(url);
 
     return apiObj;
 }
