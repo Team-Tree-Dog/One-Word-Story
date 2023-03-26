dda1a0e47fe6731168f2505e45d3a9cd50bafebd
cotopaxi64 <daniel.honciuc@mail.utoronto.ca>
2023-03-26 10:55:16 -0400

    Y'all sure that was the only test?

--- src/test/java/net/onewordstory/core/usecases/pull_game_ended/PgeInteractorTests.java
+++ src/test/java/net/onewordstory/core/usecases/pull_game_ended/PgeInteractorTests.java
@@ -1,54 +1,82 @@
 package net.onewordstory.core.usecases.pull_game_ended;
 
+import net.onewordstory.core.entities.Player;
+import net.onewordstory.core.entities.games.Game;
+import net.onewordstory.core.entities.statistics.AllPlayerNamesStatistic;
+import net.onewordstory.core.entities.validity_checkers.ValidityCheckerFacade;
+import net.onewordstory.core.exceptions.InvalidWordException;
+import net.onewordstory.core.usecases.Response;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import net.onewordstory.core.usecases.GameDTO;
+import net.onewordstory.core.usecases.PlayerDTO;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+
 public class PgeInteractorTests {
 
-    // TODO: Rewrite these tests, PGE changed drastically jjjj
-//    // PgeInputBoundary, PgeInputData, TestOutputBoundary used to test PgeInteractor
-//    private PgeInputBoundary pgeib;
-//    private PgeInputData pgeid;
-//    private final TestOutputBoundary testOutputBoundary = new TestOutputBoundary();
-//
-//    // Implementation of output boundary for testing built data
-//    private static class TestOutputBoundary implements PgeOutputBoundary {
-//
-//        ArrayList<String> builtData = new ArrayList<>();
-//
-//        @Override
-//        public void notifyGameEnded(PgeOutputData data) {
-//            builtData.add(data.getPlayerStatDTOs()[0]);
-//            builtData.add(data.getPlayerStatDTOs()[1]);
-//        }
-//    }
-//
-//    @BeforeEach
-//    public void setUp() {
-//        // Add players for a list of players
-//        Player p1 = new Player("p1", "1");
-//        Player p2 = new Player("p2", "2");
-//
-//        // Instantiate pgeInputBoundary
-//        pgeib = new PgeInteractor(testOutputBoundary,
-//                (storyString, publishUnixTimeStamp, authorDisplayNames) -> null);
-//
-//        // Instantiate pgeInputData with list of players
-//        pgeid = new PgeInputData(new ArrayList<>(2));
-//        pgeid.getPlayers().add(0, p1);
-//        pgeid.getPlayers().add(1, p2);
-//    }
-//
-//    @AfterEach
-//    public void tearDown() {}
-//
-//    /**
-//     * Test that the data is built correctly (the proper player ids were extracted)
-//     */
-//    @Test
-//    @Timeout(1000)
-//    public void testBuiltData() {
-//        ArrayList<String> expectedPlayerIds = new ArrayList<>();
-//        expectedPlayerIds.add("1");
-//        expectedPlayerIds.add("2");
-//        pgeib.onGameEnded(pgeid);
-//        assertEquals(expectedPlayerIds, testOutputBoundary.builtData);
-//    }
+    // TODO: Rewrite these tests, PGE changed drastically
+    // PgeInputBoundary, PgeInputData, TestOutputBoundary used to test PgeInteractor
+    private PgeInputBoundary pgeib;
+    private PgeInputData pgeid;
+    private final TestOutputBoundary testOutputBoundary = new TestOutputBoundary();
+
+    // Implementation of output boundary for testing built data
+    private static class TestOutputBoundary implements PgeOutputBoundary {
+
+        ArrayList<String> builtData = new ArrayList<>();
+
+        @Override
+        public void notifyGameEnded(PgeOutputData data) {
+            builtData.add(data.getPlayerStatDTOs()[0].getPlayerId());
+            builtData.add(data.getPlayerStatDTOs()[1].getPlayerId());
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        // Add players for a list of players
+        Player p1 = new Player("p1", "1");
+        Player p2 = new Player("p2", "2");
+
+        // Instantiate pgeInputBoundary
+        pgeib = new PgeInteractor(testOutputBoundary, new PgeGatewayStory() {
+            @Override
+            public @NotNull Response saveStory(String storyString, double publishUnixTimeStamp, @Nullable Set<String> authorDisplayNames) {
+                return new Response(Response.ResCode.SUCCESS, "Response has been returned successfully");
+            }
+        });
+
+        // Instantiate pgeInputData with list of players
+        pgeid = new PgeInputData(new ArrayList<>(2), "",
+                new ArrayList<>(1), new AllPlayerNamesStatistic());
+        pgeid.getPlayers().add(0, p1);
+        pgeid.getPlayers().add(1, p2);
+    }
+
+    @AfterEach
+    public void tearDown() {}
+
+    /**
+     * Test that the data is built correctly (the proper player ids were extracted)
+     */
+    @Test
+    @Timeout(1000)
+    public void testBuiltData() {
+        ArrayList<String> expectedPlayerIds = new ArrayList<>();
+        expectedPlayerIds.add("1");
+        expectedPlayerIds.add("2");
+        pgeib.onGameEnded(pgeid);
+
+        assertEquals(expectedPlayerIds, testOutputBoundary.builtData);
+    }
 }
