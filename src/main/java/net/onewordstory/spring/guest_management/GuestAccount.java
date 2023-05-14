5536379cba103946dfa7a9269a03ef9ee1bcceba
Aleksey Panas <panasaleksey3@gmail.com>
2023-05-14 18:13:18 -0400

    Finish interceptor

--- src/main/java/net/onewordstory/spring/guest_management/GuestAccount.java
+++ src/main/java/net/onewordstory/spring/guest_management/GuestAccount.java
@@ -1,35 +1,87 @@
 package net.onewordstory.spring.guest_management;
 
-import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class GuestAccount {
 
+    public static final int EXPIRE_TIME_SECONDS = 60 * 60 * 24 * 7; // 7 days
+
     private final String id;
-    private LocalDateTime lastTouch;
     private final Map<Integer, Set<String>> upvotedTitles;
     private final Set<Integer> likedStories;
+    AtomicReference<LocalDateTime> lastTouch = new AtomicReference<>();
 
     public GuestAccount(String id, LocalDateTime lastTouch) {
         this.id = id;
-        this.lastTouch = lastTouch;
-        likedStories = new HashSet<>();
-        upvotedTitles = new HashMap<>();
+        this.lastTouch.set(lastTouch);
+        likedStories = ConcurrentHashMap.newKeySet();
+        upvotedTitles = new ConcurrentHashMap<>();
     }
 
     /**
      * @return The date time of the last time a user did something using this account
      * (every web request should touch the requester's account)
      */
-    public LocalDateTime getLastTouch() { return lastTouch; }
+    public LocalDateTime getLastTouch() { return lastTouch.get(); }
+
+    /**
+     * Refresh expiration date by settings the last touch to now
+     */
+    public void touch() { lastTouch.set(LocalDateTime.now()); }
+
+    /**
+     * @return if this account has expired
+     */
+    public boolean isExpired() {
+        return lastTouch.get().until(LocalDateTime.now(), ChronoUnit.SECONDS) > EXPIRE_TIME_SECONDS;
+    }
 
     /**
      * @return ID of this account
      */
     public String getId() { return id; }
+
+    public boolean hasLikedStory(int storyId) {
+        return likedStories.contains(storyId);
+    }
+
+    public void setLikedStory(int storyId) {
+        likedStories.add(storyId);
+    }
+
+    public boolean hasUpvotedTitle(int storyId, String title) {
+        Set<String> titles = upvotedTitles.get(storyId);
+        if (titles != null) {
+            return titles.contains(title);
+        }
+        return false;
+    }
+
+    /*TODO: It should be noted that this method is not thread safe in some very rare cases:
+    * If the titles set is detected to be null but is subsequently added before the if block
+    * begins execution, then the contents will be overwritten. Similarly, if the key is detected
+    * to exist but is subsequently removed before the else block executes, the title will fail to
+    * be added and an exception will be thrown. A lock encompassing the entire method execution
+    * would solve these issues. This error is however very unlikely since the same account should
+    * rarely execute concurrent operations.
+    * */
+    public void setUpvotedTitle(int storyId, String title) {
+        Set<String> titles = upvotedTitles.get(storyId);
+        if (titles == null) {
+            Set<String> set = ConcurrentHashMap.newKeySet();
+            set.add(title);
+            upvotedTitles.put(storyId, set);
+        } else {
+            upvotedTitles.get(storyId).add(title);
+        }
+    }
 }
--- src/main/java/net/onewordstory/spring/guest_management/GuestAccountManager.java
+++ src/main/java/net/onewordstory/spring/guest_management/GuestAccountManager.java
@@ -5,25 +5,64 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import net.onewordstory.core.usecases.like_story.LsGatewayGuestAccounts;
 import net.onewordstory.core.usecases.upvote_title.UtGatewayGuestAccounts;
+import org.example.ANSI;
+import org.example.Log;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.UUID;
 
+/**
+ * Acts as a spring interceptor and manages guest accounts.
+ * A cookie is automatically set to each client to store a uuid of a guest
+ * account, which links to an account object in the server (here). The cookies
+ * are set to never expire (10 years expire time) while accounts expire within a
+ * few days. Every request refreshes the account's expiry time
+ */
 @Component
 public class GuestAccountManager implements HandlerInterceptor, LsGatewayGuestAccounts, UtGatewayGuestAccounts {
 
-    public static String COOKIE_NAME = "account_id";
-    public static int COOKIE_EXPIRE_TIME = 60 * 60 * 24 * 365 * 10; // 10 years
+    public static final String COOKIE_NAME = "account_id";
+    public static final int COOKIE_EXPIRE_TIME_SECONDS = 60 * 60 * 24 * 365 * 10; // 10 years
 
     private final Map<String, GuestAccount> uuidToAcc = new ConcurrentHashMap<>();
     private final Lock lock = new ReentrantLock();
+    private AtomicBoolean isExpiryWorkerRunning = new AtomicBoolean(false);
+
+    /**
+     * Thread which loops the account map and deletes all expired accounts
+     */
+    private class ExpiryWorker implements Runnable {
+        @Override
+        public void run() {
+            isExpiryWorkerRunning.set(true);
+            Log.sendMessage(ANSI.YELLOW, "Expiry Worker",
+                    ANSI.CYAN, "Started");
+
+            Set<String> keys = uuidToAcc.keySet();
+
+            for (String k: keys) {
+                if (uuidToAcc.get(k).isExpired()) {
+                    uuidToAcc.remove(k);
+                    Log.sendMessage(ANSI.YELLOW, "Expiry Worker",
+                            ANSI.CYAN, "Removing account " + k);
+                }
+            }
+
+            isExpiryWorkerRunning.set(false);
+            Log.sendMessage(ANSI.YELLOW, "Expiry Worker",
+                    ANSI.CYAN, "Finished");
+        }
+    }
 
     /**
      *
@@ -36,15 +75,16 @@ public class GuestAccountManager implements HandlerInterceptor, LsGatewayGuestAc
     public boolean preHandle(@NotNull HttpServletRequest request,
                              @NotNull HttpServletResponse response,
                              @NotNull Object handler) {
-        System.out.println("Request INTERCEPTED");
-        System.out.println(request);
+        Log.sendSocketGeneral("Interceptor", "Request Intercepted");
 
-        //lock.lock();
+        // Puts out a thread to delete expired accounts
+        if (!isExpiryWorkerRunning.get()) {
+            new Thread(new ExpiryWorker()).start();
+        }
 
         // Look for the account cookie
         Cookie cookie = null;
         Cookie[] cookies = request.getCookies();
-
         if (cookies != null) {
             for (Cookie c : cookies) {
                 if (c.getName().equals(COOKIE_NAME)) {
@@ -53,42 +93,70 @@ public class GuestAccountManager implements HandlerInterceptor, LsGatewayGuestAc
             }
         }
 
+        // Make new account and add cookie
         if (cookie == null) {
+            response.addCookie(new Cookie("account_id", makeAccount()));
+            Log.sendSocketGeneral("Interceptor", "New account made");
+        }
 
-        } else {
+        else {
             // Tries to get associated account
             GuestAccount targetAcc = uuidToAcc.get(cookie.getValue());
 
-            // Account doesn't exist
-            if (targetAcc == null) {
-
-            } else {
-
+            // Account doesn't exist or is expired
+            if (targetAcc == null || targetAcc.isExpired()) {
+                response.addCookie(new Cookie("account_id", makeAccount()));
+            }
+            // Account was found
+            else {
+                targetAcc.touch();
+                Log.sendSocketGeneral("Interceptor", "Account touched");
             }
         }
-
-        //lock.unlock();
 
         return true;
     }
 
+    /**
+     * Add a new account to the map and return the uuid
+     */
+    private String makeAccount() {
+        String uuid = UUID.randomUUID().toString();
+        uuidToAcc.put(uuid, new GuestAccount(uuid, LocalDateTime.now()));
+        return uuid;
+    }
+
     @Override
     public boolean hasLikedStory(String guestAccId, int storyId) {
+        GuestAccount acc = uuidToAcc.get(guestAccId);
+        if (acc != null) {
+            acc.hasLikedStory(storyId);
+        }
         return false;
     }
 
     @Override
     public void setLikedStory(String guestAccId, int storyId) {
-
+        GuestAccount acc = uuidToAcc.get(guestAccId);
+        if (acc != null) {
+            acc.setLikedStory(storyId);
+        }
     }
 
     @Override
     public boolean hasUpvotedTitle(String guestAccountId, int storyId, String title) {
+        GuestAccount acc = uuidToAcc.get(guestAccountId);
+        if (acc != null) {
+            acc.hasUpvotedTitle(storyId, title);
+        }
         return false;
     }
 
     @Override
     public void setUpvotedTitle(String guestAccountId, int storyId, String title) {
-
+        GuestAccount acc = uuidToAcc.get(guestAccountId);
+        if (acc != null) {
+            acc.setUpvotedTitle(storyId, title);
+        }
     }
 }
